b38c91bd0360a7fc65e8392b41f1213d85506546
Damisola Morawo <morawodamisolataiwo77@gmail.com>
2023-12-12 10:09:34 +0100

    Implementing ComposeBasics in android developer documentation

---
+++ app/src/main/java/com/damisola/composebasics/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.damisola.composebasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.damisola.composebasics.ui.theme.ComposeBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeBasicsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+@Composable
+fun BasicImage() {
+    Column(modifier = Modifier, verticalArrangement = Arrangement.Top) {
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = "compose background"
+        )
+    }
+}
+    @Composable
+    fun BasicArticles() {
+        Column(modifier = Modifier, verticalArrangement = Arrangement.Center) {
+
+            Text(
+                text = "Jetpack Compose tutorial",
+                fontSize = 24.sp,
+                modifier = Modifier.padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                    bottom = 16.dp,
+                )
+            )
+            Text(
+                text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+                modifier = Modifier
+                    .padding(
+                        start = 16.dp,
+                        end = 16.dp,
+                    )
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Justify
+            )
+            Text(
+                text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+                modifier = Modifier.padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                    bottom = 16.dp
+                )
+            )
+        }
+    }
+
+
+@Preview(showBackground = true)
+@Composable
+fun BasicScreenPreview() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+
+    ){
+    BasicArticles()
+        BasicImage()
+    }
+
+
+
+       
+
+}
