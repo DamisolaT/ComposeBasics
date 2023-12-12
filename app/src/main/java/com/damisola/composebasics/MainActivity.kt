6b668f0de86cb1aebacd870d6864e93a0f215ee1
Damisola Morawo <morawodamisolataiwo77@gmail.com>
2023-12-12 16:48:03 +0100

    Implementing ComposeBasics in android developer documentation

--- app/src/main/java/com/damisola/composebasics/MainActivity.kt
+++ app/src/main/java/com/damisola/composebasics/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -33,56 +34,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                BasicArticles()
                 }
             }
         }
     }
 }
-@Composable
-fun BasicImage() {
-    Column(modifier = Modifier, verticalArrangement = Arrangement.Top) {
-        Image(
-            painter = painterResource(id = R.drawable.bg_compose_background),
-            contentDescription = "compose background"
-        )
-    }
-}
-    @Composable
-    fun BasicArticles() {
-        Column(modifier = Modifier, verticalArrangement = Arrangement.Center) {
 
-            Text(
-                text = "Jetpack Compose tutorial",
-                fontSize = 24.sp,
-                modifier = Modifier.padding(
-                    start = 16.dp,
-                    end = 16.dp,
-                    top = 16.dp,
-                    bottom = 16.dp,
-                )
-            )
-            Text(
-                text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
-                modifier = Modifier
-                    .padding(
-                        start = 16.dp,
-                        end = 16.dp,
-                    )
-                    .fillMaxWidth(),
-                textAlign = TextAlign.Justify
-            )
-            Text(
-                text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
-                modifier = Modifier.padding(
-                    start = 16.dp,
-                    end = 16.dp,
-                    top = 16.dp,
-                    bottom = 16.dp
-                )
-            )
-        }
-    }
 
 
 @Preview(showBackground = true)
@@ -94,7 +52,6 @@ fun BasicScreenPreview() {
 
     ){
     BasicArticles()
-        BasicImage()
     }
 
 
